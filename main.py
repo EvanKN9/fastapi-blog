e229708cd74eb6d0bdfa758384b1ff16b5d6ad90
EvanKN9 <evann1151@gmail.com>
2026-09-06 16:23:57 -0700

    Initial commit
    Setup with HTML frontend using Jinja2 Template

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Evan",
+        "title": "FastAPI Blog",
+        "content": "This is a project to develop my web application knowledge"
+    }
+]
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="post")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"},)
+    
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
